83e1955183d34e0f9088404ce52100f0083062d1
claurine <laurine.camuset@ynov.com>
2026-02-24 12:04:59 +0100

    feat(login-page): Initialisation of the login/sign up page
    # Conflicts:
    #	.gitignore
    #	index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // Simple PHP router for demonstration
 if ($_SERVER['REQUEST_URI'] === '/' || $_SERVER['REQUEST_URI'] === '/index.php') {
-    include __DIR__ . '/public/index.html';
+    include __DIR__ . '/public/login.html';
     exit;
 }
 // Add more routes as needed
